830ebafa637496d03717f723efc76268de1be822
Caique Neves <t_cfneves@trf5.gov.br>
2018-04-03 18:46:46 -0300

    EndPoint /pedidos/[id] disponibilizado

--- src/main/java/com/caiqueferreira/cursomc/domain/Pagamento.java
+++ src/main/java/com/caiqueferreira/cursomc/domain/Pagamento.java
@@ -11,6 +11,8 @@ import javax.persistence.MapsId;
 import javax.persistence.OneToOne;
 
 import com.caiqueferreira.cursomc.domain.enums.EstadoPagamento;
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 @Entity
 @Inheritance(strategy=InheritanceType.JOINED)
@@ -22,6 +24,7 @@ public abstract class Pagamento implements Serializable {
 	private Integer ID;
 	private Integer estado;
 	
+	@JsonBackReference
 	@OneToOne
 	@JoinColumn(name="pedido_id")
 	@MapsId
---
+++ src/main/java/com/caiqueferreira/cursomc/service/PedidoService.java
@@ -0,0 +1,25 @@
+package com.caiqueferreira.cursomc.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.caiqueferreira.cursomc.domain.Pedido;
+import com.caiqueferreira.cursomc.repositories.PedidoRepository;
+import com.caiqueferreira.cursomc.service.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+    
+	@Autowired
+	private PedidoRepository repo;
+	
+	public Pedido buscar(Integer id) {
+		Pedido obj = repo.findOne(id);
+		if (obj == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado Id:" +id 
+					+ ", Tipo: " + Pedido.class.getName());
+		}
+		
+		return obj;
+	}
+}
